ad93be982b9eb56d82022824b69f8c3879cef238
Magdalena Kulakowska <magdalena.michalskaa@gmail.com>
2024-09-19 20:07:33 +0200

    JUnit

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -13,4 +13,37 @@ public class CalculatorTestSuite {
         int sumResult = calculator.sum(a, b);
         assertEquals(13, sumResult);
     }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subtractResult = calculator.subtract(a, b);
+        assertEquals(-3, subtractResult);
+    }
+
+    @Test
+    public void testSquaringForNegativeNumber() {
+        Calculator calculator = new Calculator();
+        int a = -5;
+        double squaringResult = calculator.squaring(a);
+        assertEquals(25, squaringResult, 0.01);
+    }
+
+    @Test
+    public void testSquaringForZero() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        double squaringResult = calculator.squaring(a);
+        assertEquals(0, squaringResult, 0.01);
+    }
+
+    @Test
+    public void testSquaringForPositiveNumber() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        double squaringResult = calculator.squaring(a);
+        assertEquals(25, squaringResult, 0.01);
+    }
 }
