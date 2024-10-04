f6754da2e4e983973eafe89021fa8bb765795092
Magdalena Kulakowska <magdalena.michalskaa@gmail.com>
2024-10-04 12:46:16 +0200

    immutable

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskApplication.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskApplication {
+    public static void main(String[] args) {
+        Task task = new Task("Create new immutable class", 1);
+    }
+}
