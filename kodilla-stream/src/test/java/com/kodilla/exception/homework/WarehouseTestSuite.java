a23ed40f30f1f0b2c49b97d70485e2acd8b65148
Magdalena Kulakowska <magdalena.michalskaa@gmail.com>
2024-10-15 12:32:52 +0200

    Exceptions tests

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.exception.homework;
+
+import com.kodilla.exception.AirportNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WarehouseTestSuite {
+
+    @Test
+    public void testGetOrder() throws OrderDoesntExistException {
+        //given
+        Warehouse warehouse = new Warehouse();
+
+        warehouse.addOrder(new Order("1"));
+        warehouse.addOrder(new Order("65"));
+        warehouse.addOrder(new Order("32"));
+
+        //when
+        Order order = warehouse.getOrder("32");
+
+        //then
+        assertEquals(new Order("32"), order);
+    }
+
+    @Test
+    public void testGetOrder_withException(){
+        //given
+        Warehouse warehouse = new Warehouse();
+
+        warehouse.addOrder(new Order("1"));
+        warehouse.addOrder(new Order("65"));
+        warehouse.addOrder(new Order("32"));
+
+        //when
+
+        //then
+        assertThrows(OrderDoesntExistException.class, () -> warehouse.getOrder("5"));
+    }
+}
