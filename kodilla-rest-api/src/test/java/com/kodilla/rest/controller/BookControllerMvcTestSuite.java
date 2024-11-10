da04678ed1c243591c8d866062531557e666a645
Magdalena Kulakowska <magdalena.michalskaa@gmail.com>
2024-11-10 20:03:29 +0100

    Rest API - unit tests for controller

--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerMvcTestSuite.java
@@ -1,4 +1,72 @@
 package com.kodilla.rest.controller;
 
+import com.google.gson.Gson;
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.awt.print.Book;
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(BookController.class)
 public class BookControllerMvcTestSuite {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void shouldFetchBooks() throws Exception {
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("Title 1", "Author 1"));
+        booksList.add(new BookDto("Title 2", "Author 2"));
+        Mockito.when(bookService.getBooks()).thenReturn(booksList);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/books")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)));
+    }
+
+    @Test
+    public void shouldAddBook() throws Exception {
+        BookDto book = new BookDto("Title 1", "Author 1");
+        Mockito.doNothing().when(bookService).addBook(book);
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(book);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/books")
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+    }
+
+    @Test
+    public void shouldRemoveBook() throws Exception {
+        BookDto book = new BookDto("Title 1", "Author 1");
+        Mockito.doNothing().when(bookService).removeBook(book);
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(book);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/books")
+                .content(jsonString)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is(200));
+
+    }
 }
--- kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
+++ kodilla-rest-api/src/test/java/com/kodilla/rest/controller/BookControllerTestSuite.java
@@ -1,4 +1,72 @@
+package com.kodilla.rest.controller;
+
+import com.kodilla.rest.domain.BookDto;
+import com.kodilla.rest.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class BookControllerTestSuite {
-  
+
+    @Test
+    public void shouldFetchBooks() {
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(new BookDto("Title 1", "Author 1"));
+        booksList.add(new BookDto("Title 2", "Author 2"));
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+        List<BookDto> result = bookController.getBooks();
+
+        assertThat(result).hasSize(2);
+        assertEquals(result, booksList);
+        Mockito.verify(bookServiceMock).getBooks();
+    }
+
+    @Test
+    public void shouldAddBook() {
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        BookDto book = new BookDto("Title 1", "Author 1");
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(book);
+        Mockito.doNothing().when(bookServiceMock).addBook(book);
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+        bookController.addBook(book);
+        List<BookDto> result = bookController.getBooks();
+
+        assertThat(result).hasSize(1);
+        assertEquals(result, booksList);
+        Mockito.verify(bookServiceMock).addBook(book);
+    }
+
+    @Test
+    public void shouldRemoveBook() {
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookController bookController = new BookController(bookServiceMock);
+        BookDto book = new BookDto("Title 1", "Author 1");
+        BookDto book2 = new BookDto("Title 2", "Author 2");
+        List<BookDto> booksList = new ArrayList<>();
+        booksList.add(book);
+        Mockito.doNothing().when(bookServiceMock).addBook(book);
+        Mockito.doNothing().when(bookServiceMock).removeBook(book);
+        Mockito.when(bookServiceMock.getBooks()).thenReturn(booksList);
+
+        bookController.addBook(book);
+        bookController.addBook(book2);
+        bookController.removeBook(book2);
+        List<BookDto> result = bookController.getBooks();
+
+        assertThat(result).hasSize(1);
+        assertEquals(result, booksList);
+        Mockito.verify(bookServiceMock).removeBook(book2);
+    }
+
 }
