90cfd9d88baaf5b4f7ba5cbec9b79c84dbad32dd
Magdalena Kulakowska <magdalena.michalskaa@gmail.com>
2024-11-25 14:21:36 +0100

    Selenium part 2

---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -0,0 +1,28 @@
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Arrays;
+
+public class AllegroTestingApp {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.pl/");
+
+        WebElement categoryCombo = driver.findElement(By.xpath("//*[@id=\"gh-cat-box\"]/select"));
+        Select categorySelect = new Select(categoryCombo);
+        categorySelect.selectByIndex(9);
+        WebElement searchField = driver.findElement(By.xpath("//*[@id=\"gh-ac-box2\"]/input"));
+        searchField.sendKeys("Mavic mini");
+        WebElement searchButton = driver.findElement(By.xpath("//*[contains(@class, \"gh-sch-btn\")]/input"));
+        searchButton.click();
+    }
+}
---
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/login/LoginTestingApp.java
@@ -0,0 +1,25 @@
+package com.kodilla.selenium.login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class LoginTestingApp {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.kodilla.com/pl/test/login/");
+
+        WebElement emailField = driver.findElement(By.xpath("//html/body/section/form/div[1]/input"));
+        emailField.sendKeys("test@kodilla.com");
+        WebElement passwordField = driver.findElement(By.xpath("//html/body/section/form/div[2]/input"));
+        passwordField.sendKeys("kodilla123");
+        WebElement submitButton = driver.findElement(By.xpath("//html/body/section/form/button"));
+        submitButton.submit();
+    }
+}
